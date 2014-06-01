4f4b33e78196b89997b94a297b2cc00a8b5dbb71
Michael Slater <mcslater@mcslater-K55A.(none)>
2014-06-01 12:23:00 -0400

    add directory as command line arg

--- cleanmp3.py
+++ cleanmp3.py
@@ -1,11 +1,10 @@
 import eyeD3
 import glob
+import sys
 
-indir = '/home/mcslater/Music/NIG*.mp3'
+indir = sys.argv[1]
 
 for fname in glob.glob(indir):
 	tag = eyeD3.Tag()
 	tag.link(fname)
 	print fname
-	tag.setArtist('Turtles')
-	print tag.getArtist()
