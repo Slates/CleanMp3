baa11744978da91008e2cf79fdc3e42c402c0ff1
Michael Slater <mcslater@mcslater-K55A.(none)>
2014-06-01 15:24:24 -0400

    list files

--- cleanmp3.py
+++ cleanmp3.py
@@ -2,9 +2,12 @@ import eyeD3
 import glob
 import sys
 
-indir = sys.argv[1]
+indir = sys.argv[1] + "*"
+# indir = "/home/mcslater/Music/*.mp3"
 
 for fname in glob.glob(indir):
-	tag = eyeD3.Tag()
-	tag.link(fname)
-	print fname
+	if fname.endswith('.mp3'):
+		tag = eyeD3.Tag()
+		tag.link(fname)
+		songtitle = tag.getArtist() + " - " + tag.getTitle()
+		print songtitle
--- interface.py
+++ interface.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
-import wx
+# buttons.py
 
-app = wx.App(False)
-frame = wx.Frame(None,"mainframe", "Clean Mp3")
-frame.Show(True)
+import wx
+import os
+
+APP_SIZE_X = 400
+APP_SIZE_Y = 300
+   
+class MyButtons(wx.Dialog):
+	def __init__(self, parent, id, title):
+		wx.Dialog.__init__(self, parent, id, title, size=(APP_SIZE_X, APP_SIZE_Y))
+
+		wx.Button(self,1, label = 'Print Mp3s', pos = (150,160), size = (100,50))
+		wx.Button(self,2, 'Random Move', pos = (0, 0), size = (1, 1))
+
+		self.Bind(wx.EVT_BUTTON, self.OnPrintMp3s, id=1)
+
+		self.Centre()
+		self.ShowModal()
+		self.Destroy()
+
+	def OnPrintMp3s(self, event):
+		os.system("python cleanmp3.py /home/mcslater/Music/")
+
+app = wx.App(0)
+MyButtons(None, -1, 'Clean Mp3')
 app.MainLoop()
